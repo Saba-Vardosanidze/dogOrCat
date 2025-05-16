2c63b8253717911418ee99de3ca47fe7c10e0b64
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-05-17 00:03:12 +0400

    Update Quiz.tsx

--- src/components/quiz/Quiz.tsx
+++ src/components/quiz/Quiz.tsx
@@ -1,23 +1,40 @@
 import {useQuery} from "@tanstack/react-query";
 import {catOrDogApi} from "../../api/api";
+import {useState} from "react";
 const Quiz = () => {
   const {data, isLoading, refetch, error} = useQuery({
     queryKey: ["catDog"],
     queryFn: catOrDogApi,
   });
+  const [score, setScore] = useState(0);
+
+  const scoreUp = (isDog) => {
+    if (isDog) {
+      setScore((prev) => prev + 1);
+    } else {
+      setScore((prev) => prev - 1);
+    }
+    refetch();
+  };
+
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>error</p>;
   return (
-    <div className="flex gap-[100px] m-auto mt-[100px] max-w-[500px]">
-      {data?.map((res, index) => (
-        <div key={index}>
-          <img
-            className="blur-sm w-[200px] h-[200px] cursor-pointer"
-            src={res.url}
-            alt="dogOrCat"
-          />
-        </div>
-      ))}
+    <div className="flex flex-col gap-[50px] m-auto mt-[100px] max-w-[500px]">
+      <p>რომელია ძაღლის ფოტო:</p>
+      <div className="flex gap-[100px] m-auto mt-[10px] max-w-[500px]">
+        {data?.map((res, index) => (
+          <div key={index}>
+            <img
+              onClick={() => scoreUp(res.isDog)}
+              className="blur-sm w-[200px] h-[200px] cursor-pointer"
+              src={res.url}
+              alt="dogOrCat"
+            />
+          </div>
+        ))}
+      </div>
+      <p className="font-bold text-[18px]">score: {score}</p>
     </div>
   );
 };
